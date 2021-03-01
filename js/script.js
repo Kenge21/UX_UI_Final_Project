3c5193b1c7ebf75adc564ef49af44c28ac823373
Kenge21 <70163821+Kenge21@users.noreply.github.com>
2021-02-28 23:42:42 -0500

    homepage updated
    hero, collection, newsletter, series containers added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//$(document).ready(function(){
+//    $(".dropdown").hover(function(){
+//        var dropdownMenu = $(this).children(".dropdown-menu");
+//        console.log('doing thing 1');
+//        ..if(!dropdownMenu.is(":visible")){
+//            console.log('doing thing 2');
+//            dropdownMenu.toggleClass("open");
+//        }
+//    });
+//});     
